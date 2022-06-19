8614453fd2e63e86292e443b3be769621f860632
Welliton Gervickas <wellitogervickas@gmail.com>
2022-06-19 18:10:10 -0300

    fix: blog route not found

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,30 +4,36 @@ import BlogContainer from '@components/Blog/Container'
 import services from '@modules/services'
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-	const { page = 1, categories: queryCategories = undefined } = context.query
+	try {
+		const { page = 1, categories: queryCategories = undefined } = context.query
 
-	const [postsResult, categoriesResult] = await Promise.all([
-		services.blog.posts({
-			page,
-			categories: queryCategories,
-		}),
-		services.blog.categories(),
-	])
+		const [postsResult, categoriesResult] = await Promise.all([
+			services.blog.posts({
+				page,
+				categories: queryCategories,
+			}),
+			services.blog.categories(),
+		])
 
-	const { posts, totalPages } = postsResult
+		const { posts, totalPages } = postsResult
 
-	const { categories } = categoriesResult
+		const { categories } = categoriesResult
 
-	return {
-		props: {
-			title: 'Blog',
-			description:
-				'Learn about cryptocurrency, NFTs, and blockchain, discover our latest product updates, partnership announcements, user stories, and more',
-			posts,
-			categories,
-			page,
-			totalPages,
-		},
+		return {
+			props: {
+				title: 'Blog',
+				description:
+					'Learn about cryptocurrency, NFTs, and blockchain, discover our latest product updates, partnership announcements, user stories, and more',
+				posts,
+				categories,
+				page,
+				totalPages,
+			},
+		}
+	} catch (error) {
+		return {
+			notFound: true,
+		}
 	}
 }
 
@@ -39,22 +45,15 @@ interface Blog {
 	totalPages: number
 }
 
-const Blog: NextPage<Blog> = ({ posts, categories, page, totalPages }) => {
-	try {
-		return (
-			<div className="space-y-12 py-20 container">
-				<BlogContainer
-					posts={posts}
-					categories={categories}
-					totalPages={totalPages}
-					currentPage={page}
-				/>
-			</div>
-		)
-	} catch (error) {
-		console.log(error)
-		return <div />
-	}
-}
+const Blog: NextPage<Blog> = ({ posts, categories, page, totalPages }) => (
+	<div className="space-y-12 py-20 container">
+		<BlogContainer
+			posts={posts}
+			categories={categories}
+			totalPages={totalPages}
+			currentPage={page}
+		/>
+	</div>
+)
 
 export default Blog
