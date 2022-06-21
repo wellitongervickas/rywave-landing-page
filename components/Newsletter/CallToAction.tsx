a2698854384b52a65c0d8f662de5642c3b8a5a5e
Welliton Gervickas <wellitogervickas@gmail.com>
2022-06-21 07:32:59 -0300

    fix: text size cta newsletter

--- components/Newsletter/CallToAction.tsx
+++ components/Newsletter/CallToAction.tsx
@@ -7,8 +7,8 @@ interface NewsletterCTA {}
 
 const NewsletterCTA: FC<NewsletterCTA> = () => (
 	<CardWrapper>
-		<div className="mx-auto flex w-full md:max-w-[70%] lg:max-w-[50%] flex-col items-center justify-center space-y-6 p-12 text-center">
-			<h3 className="text-2xl font-bold lg:text-3xl">
+		<div className="mx-auto flex w-full flex-col items-center justify-center space-y-6 p-12 text-center md:max-w-[70%] lg:max-w-[50%]">
+			<h3 className="text-base font-bold md:text-2xl lg:text-3xl">
 				Subscribe our Newsletter
 			</h3>
 			<NewsletterForm placeholder="Type your e-mail" />
