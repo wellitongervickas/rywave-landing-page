509459fa1a4f642180624057876a8bc1e0c2942f
Welliton Gervickas <wellitogervickas@gmail.com>
2022-06-25 08:22:09 -0300

    hotfix: notion link

--- components/Homepage/Team/Action.tsx
+++ components/Homepage/Team/Action.tsx
@@ -18,7 +18,7 @@ const TeamAction: FC<TeamAction> = ({ className }) => (
 	>
 		<div className="flex flex-col space-y-4 space-x-0 lg:flex-row lg:space-y-0 lg:space-x-4">
 			<a
-				href="https://indigo-operation-662.notion.site/The-Rywave-Team-2fce5e1af3f5465f8729b6d9ce47d445"
+				href="/contact"
 				target="_blank"
 				rel="noopener noreferrer"
 				className={classnames.merge([
